44f19d30fcebacc7eb465e3e6d1cb0dda43e19dc
Gabriel Marini <gabrielmarini12@icloud.com>
2024-08-01 21:23:52 -0300

    realizado a função de aparecer o botao arrow up e ao clicar subir ao topo da página.

--- src/components/arrowUp/index.tsx
+++ src/components/arrowUp/index.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { FaArrowUp } from "react-icons/fa";
 
 export function ArrowUp() {
+  const [pageYPosition, setPageYPosition] = useState(0);
+
+  function getPageYAfterScroll() {
+    setPageYPosition(window.scrollY);
+  }
+
+  window.addEventListener("scroll", getPageYAfterScroll);
+
+  function pageUp() {
+    window.scroll(0, 0);
+  }
+
   return (
-    <div className="fixed bottom-5 right-3 bg-green-950 p-2 rounded-lg text-white cursor-pointer">
-      <FaArrowUp size={18} />
-    </div>
+    <>
+      {pageYPosition > 900 && (
+        <div className="fixed bottom-5 right-3 bg-green-950 p-2 rounded-lg text-white cursor-pointer">
+          <FaArrowUp size={18} onClick={pageUp} />
+        </div>
+      )}
+    </>
   );
 }
