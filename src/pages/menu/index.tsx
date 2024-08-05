87077816a8f339d61e837df451e1bc7c43a832d8
Gabriel Marini <gabrielmarini12@icloud.com>
2024-08-04 21:18:58 -0300

    ajuste no layout do menu.

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,75 +1,93 @@
 import { Container } from "../../components/container";
+import menuBanner from "../../assets/menu-banner.jpg";
+import coffee from "../../assets/coffee.jpg";
+import carameloCoffee from "../../assets/caramelocoffee.jfif";
+import morangoCoffee from "../../assets/coffeemorango.jfif";
+import milkCoffee from "../../assets/milkcoffee.jfif";
 
 export function Menu() {
   return (
-    <Container>
-      <div className="flex flex-col mt-10 mb-10">
-        <h1 className="text-center uppercase text-3xl font-bold text-green-950 mb-10 italic  ">
-          Menu
-        </h1>
-
-        <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100 py-8">
-          <div className="items-center flex flex-col ">
-            <img
-              className="w-28"
-              src="https://www.starbucks.com.br/public/img/icons/starbucks-nav-logo.svg"
-              alt=""
-            />
-          </div>
-
-          <div className="flex flex-col justify-center items-center gap-2">
-            <p>Café com leite</p>
-            <p>Descrição</p>
-            <p>Valor</p>
-          </div>
+    <>
+      <div className="grid grid-cols-2 rounded-md bg-green-100 mb-6 w-full max-w-[1400px] mx-auto">
+        <div className="items-center flex flex-col ">
+          <img
+            src={menuBanner}
+            alt="Foto de 2 cafés"
+            className="rounded-l-md"
+          />
         </div>
 
-        <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100 py-8">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <p>Café com leite</p>
-            <p>Descrição</p>
-            <p>Valor</p>
-          </div>
-          <div className="items-center flex flex-col">
-            <img
-              className="w-28"
-              src="https://www.starbucks.com.br/public/img/icons/starbucks-nav-logo.svg"
-              alt=""
-            />
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 mb-2  rounded-md bg-green-100 py-8">
-          <div className="items-center flex flex-col">
-            <img
-              className="w-28"
-              src="https://www.starbucks.com.br/public/img/icons/starbucks-nav-logo.svg"
-              alt=""
-            />
-          </div>
-
-          <div className="flex flex-col justify-center items-center gap-2">
-            <p>Café com leite</p>
-            <p>Descrição</p>
-            <p>Valor</p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100 py-8">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <p>Café com leite</p>
-            <p>Descrição</p>
-            <p>Valor</p>
-          </div>
-          <div className="items-center flex flex-col">
-            <img
-              className="w-28"
-              src="https://www.starbucks.com.br/public/img/icons/starbucks-nav-logo.svg"
-              alt=""
-            />
-          </div>
+        <div className="flex flex-col justify-center items-center gap-2">
+          <h1 className="text-center uppercase text-3xl font-bold text-green-950 mb-10 italic  ">
+            Cardápio
+          </h1>
+          <p className="text-xs">*Cardápio sem preço</p>
+          <p className="text-xs -mt-2">
+            Consulte os nossos baristas para saber mais
+          </p>
         </div>
       </div>
-    </Container>
+      <Container>
+        <div className="flex flex-col mb-10">
+          <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100 w-[60%] mx-auto">
+            <div className="flex flex-col ">
+              <img className=" rounded-l-md" src={coffee} alt="Café" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-2">
+              <p>Café com leite</p>
+              <p>Descrição</p>
+              <p>Valor</p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100 w-[60%] mx-auto">
+            <div className="flex flex-col justify-center items-center gap-2">
+              <p>Café com leite</p>
+              <p>Descrição</p>
+              <p>Valor</p>
+            </div>
+            <div className="flex flex-col">
+              <img
+                className="rounded-r-md"
+                src={carameloCoffee}
+                alt="Café de caramelo"
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 mb-2  rounded-md bg-green-100 w-[60%] mx-auto ">
+            <div className="flex flex-col">
+              <img
+                className="rounded-l-md"
+                src={morangoCoffee}
+                alt="Café de morango"
+              />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-2">
+              <p>Café com leite</p>
+              <p>Descrição</p>
+              <p>Valor</p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 mb-2 rounded-md bg-green-100  w-[60%] mx-auto">
+            <div className="flex flex-col justify-center items-center gap-2">
+              <p>Café com leite</p>
+              <p>Descrição</p>
+              <p>Valor</p>
+            </div>
+            <div className="flex flex-col">
+              <img
+                className="rounded-r-md"
+                src={milkCoffee}
+                alt="Café com leite"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
