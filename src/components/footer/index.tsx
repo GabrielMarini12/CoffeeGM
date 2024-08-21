0b3acb1b6032b57067ede0c2333a1f8629804bc0
Gabriel Marini <gabrielmarini12@icloud.com>
2024-08-20 22:04:28 -0300

    footer responsivo para ipad tambem

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,7 +14,7 @@ export function Footer() {
     <Container>
       <div className="w-full border border-black/10 mb-12"></div>
       <footer className="h-28 w-full mb-20">
-        <div className="flex mb-10 w-[25%] justify-between max-sm:gap-2">
+        <div className="flex mb-10 w-[25%] justify-between max-lg:gap-2">
           <Link to="https://web.whatsapp.com/" target="_blank">
             <p className="bg-black p-2 rounded-full hover:bg-black/70 hover:scale-110 transition-all cursor-pointer">
               <FaWhatsapp size={20} color="#fff" />
@@ -50,7 +50,7 @@ export function Footer() {
           </Link>
         </div>
 
-        <div className="flex justify-between w-[55%] mb-10 max-sm:w-full max-sm:flex-col">
+        <div className="flex justify-between w-[55%] mb-10 max-sm:w-full max-sm:flex-col max-lg:w-full max-lg:px-4">
           <p className="cursor-pointer hover:text-black/70">
             Política de privacidade
           </p>
